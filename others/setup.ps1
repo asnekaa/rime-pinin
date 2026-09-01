95eba4841e074511eafa39b831745f19ef025dd8
ASneka <esnakesnake@gmail.com>
2026-09-01 23:53:07 +0800

    ＋カギロイ的初始化

--- others/setup.ps1
+++ others/setup.ps1
@@ -1,23 +1,37 @@
 $ErrorActionPreference = "Stop"
 
 $root = Split-Path -Parent $PSScriptRoot
-$sub = Join-Path $root "rime-ice"
-$list = Join-Path $PSScriptRoot "rime-ice-sparse.txt"
 
-if (-not (Test-Path $list)) {
-    throw "Missing: $list"
+$subs = @(
+    @{
+        Name = "rime-ice"
+        List = "rime-ice-sparse.txt"
+    },
+    @{
+        Name = "rime-kagiroi"
+        List = "rime-kagiroi-sparse.txt"
+    }
+)
+
+foreach ($s in $subs) {
+    $sub = Join-Path $root $s.Name
+    $list = Join-Path $PSScriptRoot $s.List
+
+    if (-not (Test-Path $list)) {
+        throw "Missing: $list"
+    }
+
+    git -C $root submodule update --init --recursive $s.Name
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+    git -C $sub sparse-checkout init --no-cone
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
+    Get-Content $list |
+    Where-Object { $_.Trim() -and -not $_.Trim().StartsWith("#") } |
+    git -C $sub sparse-checkout set --stdin
+
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
-
-git -C $root submodule update --init --recursive rime-ice
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-
-git -C $sub sparse-checkout init --no-cone
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-
-Get-Content $list |
-Where-Object { $_.Trim() -and -not $_.Trim().StartsWith("#") } |
-git -C $sub sparse-checkout set --stdin
-
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
 Write-Host "Done."
